bca883168ec07191fd09305ddb5edb2484939356
arnon3339 <arnon3339@gmail.com>
2025-06-20 14:49:07 +0700

    adding input

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,9 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+# Input box
 name = st.text_input("Enter your name")
+
+# Response
+if name:
+    st.write(f"Welcome, {name}!")
